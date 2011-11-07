2ab28bb19c2a7633baca431c26dc1ec3eea774ee
Christopher Sebastian <csebastian3@gmail.com>
2011-11-07 02:52:41 -0600

    creating infrastructure to generate app and version pages

---
+++ bin/create_app_pages.py
@@ -0,0 +1,10 @@
+#!python
+
+import nomake_apps
+
+if __name__ == '__main__':
+    for app in nomake_apps.iterApps():
+        print app
+
+
+
---
+++ bin/nomake_apps.py
@@ -0,0 +1,69 @@
+
+import os, json, time, hashlib
+
+APPS_DIR = '/home/nomake/src/apps'
+SRC_WWW_DIR = '/home/nomake/src/www'
+SCREENSHOT_DIR = '/home/nomake/www/bigfiles/screenshots'
+PACKAGE_DIR = '/home/nomake/www/bigfiles/packages'
+
+
+def hashfile(afile, hasher, blocksize=65536):
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    return hasher.hexdigest()
+
+def iterAppDirs():
+    for item in os.listdir(APPS_DIR):
+        fullPath = os.path.join(APPS_DIR, item)
+        if not os.path.isdir(fullPath):
+            print 'Unexpected item in Apps Dir: %r'%(item,)
+            continue
+        yield fullPath
+
+def iterAppVersions(appDir):
+    versionsDir = os.path.join(appDir, 'versions')
+    for item in os.listdir(versionsDir):
+        fullPath = os.path.join(versionsDir, item)
+        if not item.endswith('.json'):
+            print 'Unexpected item in App Versions: %r'%(fullPath,)
+            continue
+        version = item[:-len('.json')]
+        props = json.load(open(fullPath))
+        packageFilename = props['filename']
+        packagePath = os.path.join(PACKAGE_DIR, packageFilename)
+        assert os.path.exists(packagePath), 'Package file does not exist: %r'%(packagePath,)
+        props['version'] = version
+        if 'date' not in props:
+            props['date'] = time.strftime('%Y-%M-%d', time.localtime(os.path.getmtime(packagePath)))
+        if 'hash_md5' not in props:
+            print 'Calculating md5 of %s...'%(packagePath),
+            hex = hashfile(open(packagePath, 'rb'), hashlib.md5())
+            print '"hash_md5":"%s"'%(hex,)
+            props['hash_md5'] = hex
+        if 'hash_sha1' not in props:    
+            print 'Calculating sha1 of %s...'%(packagePath),
+            hex = hashfile(open(packagePath, 'rb'), hashlib.sha1())
+            print '"hash_sha1":"%s"'%(hex,)
+            props['hash_sha1'] = hex
+        yield props
+
+def readScreenshots(appDir):
+    fullPath = os.path.join(appDir, 'screenshots.json')
+    return json.load(open(fullPath))
+
+def readAppMetadata(appDir):
+    fullPath = os.path.join(appDir, 'app.json')
+    return json.load(open(fullPath))
+
+def iterApps():
+    for appDir in iterAppDirs():
+        props = {}
+        props.update(readAppMetadata(appDir))
+        props['screenshots'] = readScreenshots(appDir)
+        props['versions'] = [v for v in iterAppVersions(appDir)]
+        yield props
+
+
+
